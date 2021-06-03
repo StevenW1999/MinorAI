cfa370dbbf03da63a8d1699a5bfd0d5bc599cdea
StevenW1999 <33688377+StevenW1999@users.noreply.github.com>
2021-06-03 18:47:09 +0200

    added cnn network

--- CNN.py
+++ CNN.py
@@ -63,3 +63,137 @@ val_y = emotions[num_train_samples:]
 
 train_data = (train_x, train_y)
 val_data = (val_x, val_y)
+
+print('Training Pixels',train_x.shape)  # ==> 4 dims -  no of images , width , height , color
+print('Training labels',train_y.shape)
+
+print('Validation Pixels',val_x.shape)
+print('Validation labels',val_y.shape)
+
+
+
+from keras.layers import Activation, Convolution2D, Dropout, Conv2D
+from keras.layers import AveragePooling2D, BatchNormalization
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import MaxPooling2D
+from keras.layers import SeparableConv2D
+from keras import layers
+from keras.regularizers import l2
+
+input_shape=(48, 48, 1)
+num_classes = 7
+
+model = Sequential()
+model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same',
+                            name='image_array', input_shape=input_shape))
+model.add(BatchNormalization())
+model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(.5))
+
+model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
+model.add(BatchNormalization())
+model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(.5))
+
+model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(.5))
+
+model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(.5))
+
+model.add(Convolution2D(filters=256, kernel_size=(3, 3), padding='same'))
+
+
+
+model.add(BatchNormalization())
+model.add(Convolution2D(filters=num_classes, kernel_size=(3, 3), padding='same'))
+model.add(GlobalAveragePooling2D())
+model.add(Activation('softmax',name='predictions'))
+
+model.summary()
+
+
+# parameters
+batch_size = 32 #Number of samples per gradient update
+num_epochs = 200 # Number of epochs to train the model.
+#input_shape = (64, 64, 1)
+verbose = 1 #per epohs  progress bar
+num_classes = 7
+patience = 50
+base_path = 'drive/Colab Notebooks/emotion/simplecnn/'
+
+
+from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+
+
+# data generator Generate batches of tensor image data with real-time data augmentation
+data_generator = ImageDataGenerator(
+                        featurewise_center=False,
+                        featurewise_std_normalization=False,
+                        rotation_range=10,
+                        width_shift_range=0.1,
+                        height_shift_range=0.1,
+                        zoom_range=.1,
+                        horizontal_flip=True)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+datasets = ['dataset2']
+num_epochs = 30
+base_path = "/content"
+for dataset_name in datasets:
+    print('Training dataset:', dataset_name)
+
+    # callbacks
+    log_file_path = dataset_name + '_emotion_training.log'
+
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping('val_loss', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
+
+    trained_models_path = base_path + dataset_name + 'simple_cnn'
+    model_names = trained_models_path + '.{epoch:02d}-{val_loss:.2f}.hdf5'  # if error "acc" in 1 line ... don't confuse check entire block since fit() generates a inner loop
+    model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1, save_best_only=True)
+    my_callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
+
+    # loading dataset
+    train_faces, train_emotions = train_data
+    history = model.fit_generator(data_generator.flow(train_faces, train_emotions,
+                                                      batch_size),
+                                  epochs=num_epochs, verbose=1
+                                  , callbacks=my_callbacks, validation_data=val_data)
+
+
+#evaluate() returns [loss,acc]
+score = model.evaluate(val_x, val_y, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1]*100)
+
+history_dict=history.history
+history_dict.keys()
+
+
+print(history_dict["accuracy"])
